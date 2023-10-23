e126755c32c1161bf56279fa3c9c3deb2f75a998
garimaneuphony <garima@neuphony.com>
2023-10-23 19:23:53 +0530

    chore: control pitch, yaw, roll of a cube using gui

--- main.js
+++ main.js
@@ -1,141 +1,143 @@
 import * as THREE from "three";
 import "./styles.css";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
 import gsap from "gsap";
 
-/* // CUBE
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector(".webgl");
+
+// Scene
 const scene = new THREE.Scene();
-// BASE OBJECT -HOLDS ALL 3D OBJECTS
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-// TO VIEW OUR SCENE = (DEGREE, ASPECT RATIO, NEAR AND FAR PLAIN)
 
-const renderer = new THREE.WebGLRenderer();
-// TO RENDER CONTENTS ON SCREEN
-renderer.setSize(window.innerWidth, window.innerHeight);
-// SET SIZE OF RENDER TARGET
-document.body.appendChild(renderer.domElement);
-// ATTACH OUTPUT TO DOM
+const gui = new GUI();
+const parameters = {
+  color: 0xffff00,
+  // spinY: () => {
+  //   gsap.to(mesh.rotation, {
+  //     duration: 4,
+  //     y: mesh.rotation.y + Math.PI * 2 - 1,
+  //   });
+  // },
+};
 
-const geometry = new THREE.BoxGeometry();
+/**
+ * Object
+ */
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+// .toNonIndexed();
+geometry.colorsNeedUpdate = true;
+
+// geometry.faces[0] = new THREE.Color(1, 0, 0);
 const material = new THREE.MeshBasicMaterial({
-  color: "red",
+  color: parameters.color,
+  vertexColors: true,
 });
-// HOW TO RENDER THAT OBJECT
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-// ADD CUBE TO THE SCENE
+const positionAttribute = geometry.getAttribute("position");
+console.log(positionAttribute);
+const colors = [];
+const color = new THREE.Color();
 
-camera.position.z = 5;
-// SET CAMERA POSITION TO 5, otherwise we will view from inside the cube and won't be able to view it
+for (let i = 0; i < positionAttribute.count; i += 3) {
+  color.set(Math.random() * 0xffffff);
 
-function animate() {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
+  colors.push(color.r, color.g, color.b);
+  colors.push(color.r, color.g, color.b);
+  colors.push(color.r, color.g, color.b);
 }
+geometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+// Debug
 
-animate();
-*/
+gui.add(mesh.rotation, "x").min(-1.5).max(1.5).step(0.1).name("Pitch (x)");
+gui.add(mesh.rotation, "y").min(-1.5).max(1.5).step(0.1).name("Yaw (y)");
+gui.add(mesh.rotation, "z").min(-1.5).max(1.5).step(0.1).name("Roll (z)");
 
-// SPHERE
-
-const scene = new THREE.Scene();
+/**
+ * Sizes
+ */
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
 
-const geometry = new THREE.SphereGeometry(5, 64, 64);
-const material = new THREE.MeshPhongMaterial({
-  color: "#00ff83",
-  // metal: true,
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-const light = new THREE.PointLight(0xffffff, 40, 100, 1.2);
-light.position.set(0, 10, 10);
-light.castShadow = true;
+//Create a PointLight and turn on shadows for the light
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 4);
+light.castShadow = true; // default false
 scene.add(light);
-// color, min, max view - beyond that it will be black
 
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+});
+
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(
-  45,
+  75,
   sizes.width / sizes.height,
   0.1,
   100
 );
-camera.position.z = 20;
+camera.position.z = 3;
+camera.position.y = 2;
 scene.add(camera);
 
-const canvas = document.querySelector(".webgl");
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(2);
-renderer.render(scene, camera);
-
-// CONTROLS
+// Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-controls.enableZoom = false;
-controls.autoRotate = true;
-controls.autoRotateSpeed = 5;
 
-window.addEventListener("resize", () => {
-  // update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-  // update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(sizes.width, sizes.height);
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
 });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-const loop = () => {
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
   controls.update();
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.01;
+
+  // Render
   renderer.render(scene, camera);
-  window.requestAnimationFrame(loop);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
 };
-loop();
 
-// Timeline magic
-const tl = gsap.timeline({ defaults: { duration: 1 } });
-tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
-tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
-tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
+tick();
 
-// Mouse Animation Color
-let mouseDown = true;
-let rgb = [];
-window.addEventListener("mousedown", () => (mouseDown = false));
-window.addEventListener("mouseup", () => (mouseDown = true));
-
-window.addEventListener("mousemove", (e) => {
-  console.log("object");
-  if (mouseDown) {
-    console.log("mouse down");
-    rgb = [
-      Math.round((e.pageX / sizes.width) * 255),
-      Math.round((e.pageY / sizes.width) * 255),
-      150,
-    ];
-    console.log(rgb);
-    // let's animate
-    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
-    gsap.to(mesh.material.color, {
-      r: newColor.r,
-      g: newColor.g,
-      b: newColor.b,
-    });
-  }
-});
+// const geometry = new THREE.SphereGeometry(5, 64, 64);
+// const material = new THREE.MeshPhongMaterial({
+//   color: "#00ff83",
+//   // metal: true,
+// });
